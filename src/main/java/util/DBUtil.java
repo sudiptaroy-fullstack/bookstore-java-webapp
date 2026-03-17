fce7d2d506a99099044d0fa8e97df541d5dd0803
Sudiptaroy-fullstack <sudiptaroyofficial21@gamil.com>
2026-03-18 01:24:58 +0530

    Remove hardcoded DB credentials , using env variables

--- src/main/java/util/DBUtil.java
+++ src/main/java/util/DBUtil.java
@@ -8,8 +8,8 @@ public class DBUtil {
     private static final String URL =
         "jdbc:mysql://localhost:3306/bookstore?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
 
-    private static final String USER = "root";
-    private static final String PASSWORD = "admin"; // or your password
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD"); // or your password
 
     public static Connection getConnection() {
         try {
